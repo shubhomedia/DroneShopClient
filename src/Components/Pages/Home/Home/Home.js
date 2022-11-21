3b1451889bc31e9b8dbae60c4da6a283505dd604
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-21 22:56:06 +0600

    Home Component Update

--- src/Components/Pages/Home/Home/Home.js
+++ src/Components/Pages/Home/Home/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Footer from '../../../Shared/Footer/Footer';
 import Navigation from '../../../Shared/Navigation/Navigation';
 import Products from '../../Products/Products/Products';
 import Banner from '../Banner/Banner';
+import ProductsShow from '../ProductsShow/ProductsShow';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
             <Banner></Banner>
-            <Products></Products>
+            <ProductsShow></ProductsShow>
         </div>
     );
 };
