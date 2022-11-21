5b3e8ad3119183934acd8dec9fad91b6a0cf0932
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-21 20:47:18 +0600

    Login Page Creation

--- src/Components/Pages/Login/Login/Login.js
+++ src/Components/Pages/Login/Login/Login.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './Login.css';
+import Avatar from '@mui/material/Avatar';
+import { Box, Container, Typography } from '@mui/material';
+
 
 
 const Login = () => {
-    return (
-        <div>
 
-        </div>
+    const singin = () => {
+        const handleSubmit = (event) => {
+            event.preventDefault();
+            const data = new FormData(event.currentTarget);
+            console.log({
+                email: data.get('email'),
+                password: data.get('password')
+            });
+        };
+    };
+
+    return (
+        <Container>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'colum',
+                    alignItems: 'center',
+                }}
+            >
+
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+
+            </Box>
+        </Container>
     );
 };
 
