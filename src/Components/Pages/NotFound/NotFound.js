bd0158604343665ccebcb7a8711a016d0f59d326
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-21 20:46:02 +0600

    404 Not Found Page Creation Done

---
+++ src/Components/Pages/NotFound/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <section class="page_404">
+                <h2>404 Not Found</h2>
+                <div class="four_zero_four_bg">
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default NotFound;
+
+
+
