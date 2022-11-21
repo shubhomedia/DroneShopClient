ac39f39671f357ee7609fb73dd0211242a07b686
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-21 20:46:19 +0600

    Router Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Components/Pages/Home/Home/Home';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NotFound from './Components/Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-    </div>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />}>
+          </Route>
+          <Route exact path='*' element={<NotFound />}>
+          </Route>
+        </Routes>
+      </Router>
+    </div >
   );
 }
 
