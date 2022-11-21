cae2ea82417665e60b873484477ef602d1f4aedb
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-21 22:55:48 +0600

    About Component Added

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
 import './About.css';
 
 const About = () => {
     return (
         <div>
-
+            <Navigation></Navigation>
+            <h2>About Page</h2>
         </div>
     );
 };
