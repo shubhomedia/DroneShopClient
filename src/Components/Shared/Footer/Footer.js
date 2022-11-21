adeb5bcf681f42da87839c021cd877c2aeb858e7
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-21 22:57:46 +0600

    Update Footer

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -1,11 +1,18 @@
+import { Typography } from '@mui/material';
+import { red } from '@mui/material/colors';
 import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
     return (
         <div>
+            <Typography
+                sx={{ textAlign: 'center', py: 2, background: 'gray', color: 'white', }}
+                variant="h5" component="h5">
+                All Rights Reserved @ DroneShop
+            </Typography>;
 
-        </div>
+        </div >
     );
 };
 
