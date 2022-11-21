18775b3f877f168d515761dee5b58404ac919bc8
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-21 22:55:33 +0600

    New Router Added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import Home from './Components/Pages/Home/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NotFound from './Components/Pages/NotFound/NotFound';
+import Login from './Components/Pages/Login/Login/Login';
+import Register from './Components/Pages/Login/Register/Register';
+import About from './Components/Pages/About/About';
+import Products from './Components/Pages/Products/Products/Products';
+import Footer from './Components/Shared/Footer/Footer';
 
 function App() {
   return (
@@ -11,9 +16,18 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />}>
           </Route>
-          <Route exact path='*' element={<NotFound />}>
+          <Route path='/about' element={<About />}>
+          </Route>
+          <Route path='/products' element={<Products />}>
+          </Route>
+          <Route path='/login' element={<Login />}>
+          </Route>
+          <Route path='/register' element={<Register />}>
+          </Route>
+          <Route path='*' element={<NotFound />}>
           </Route>
         </Routes>
+        <Footer></Footer>
       </Router>
     </div >
   );
