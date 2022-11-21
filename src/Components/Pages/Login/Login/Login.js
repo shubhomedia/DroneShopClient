67140d053e21c1243ce8e51b2961735ccffff572
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-21 22:57:30 +0600

    Update Login

--- src/Components/Pages/Login/Login/Login.js
+++ src/Components/Pages/Login/Login/Login.js
@@ -1,40 +1,97 @@
 import React from 'react';
 import './Login.css';
 import Avatar from '@mui/material/Avatar';
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Button, Container, Grid, TextField, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Link from '@mui/material/Link';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Navigation from '../../../Shared/Navigation/Navigation';
 
 
 
 const Login = () => {
-
+    const theme = createTheme();
     const singin = () => {
-        const handleSubmit = (event) => {
-            event.preventDefault();
-            const data = new FormData(event.currentTarget);
-            console.log({
-                email: data.get('email'),
-                password: data.get('password')
-            });
-        };
     };
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password')
+        });
+    }
     return (
-        <Container>
-            <Box
-                sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'colum',
-                    alignItems: 'center',
-                }}
-            >
+        <ThemeProvider theme={theme}>
+            <Navigation></Navigation>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        // display: 'flex',
+                        alignItems: 'center',
+                        justifyItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                </Box >
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >Sign In</Button>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 0, mb: 2 }}
+                    >Sign In With Google</Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/register" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
+        </ThemeProvider >
 
-                <Typography component="h1" variant="h5">
-                    Sign in
-                </Typography>
-
-            </Box>
-        </Container>
     );
 };
 
