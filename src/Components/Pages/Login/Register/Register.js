959bff041be51db338747f5493b4551f656f1579
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-21 22:57:38 +0600

    Update Register

--- src/Components/Pages/Login/Register/Register.js
+++ src/Components/Pages/Login/Register/Register.js
@@ -1,10 +1,110 @@
 import React from 'react';
 import './Register.css';
+import Avatar from '@mui/material/Avatar';
+import { Box, Button, Container, Grid, TextField, Typography } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Link from '@mui/material/Link';
+import CssBaseline from '@mui/material/CssBaseline';
+import Navigation from '../../../Shared/Navigation/Navigation';
 
 const Register = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password')
+        });
+    }
+
     return (
         <div>
+            <Navigation></Navigation>
+            <Container component="main" maxWidth="xs">
 
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        // display: 'flex',
+                        alignItems: 'center',
+                        justifyItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                </Box >
+                <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="name"
+                        label="Your Name"
+                        name="name"
+                        autoComplete="name"
+                        autoFocus
+                    />
+                    <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <TextField
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Repet Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    >Register</Button>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 0, mb: 2 }}
+                    >Sign In With Google</Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="/login" variant="body2">
+                                {"Already Have Account! Login In"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Container>
         </div>
     );
 };
