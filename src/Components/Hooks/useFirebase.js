08d60086cea32c40917e13dd27240eca46156894
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-25 07:53:36 +0600

    Firebase Code Update

--- src/Components/Hooks/useFirebase.js
+++ src/Components/Hooks/useFirebase.js
@@ -1,51 +1,51 @@
-import { useState, useEffect } from 'react';
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, getIdToken } from "firebase/auth";
-import initializeAuthentication from '../Firebase/firebase.init';
+// import { useState, useEffect } from 'react';
+// import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut, getIdToken } from "firebase/auth";
+// import initializeAuthentication from '../Firebase/firebase.init';
 
-initializeAuthentication();
+// initializeAuthentication();
 
-const useFirebase = () => {
-    const [user, setUser] = useState({});
-    const [loading, setLoading] = useState(true)
-    const auth = getAuth();
-    const googleProvider = new GoogleAuthProvider();
+// const useFirebase = () => {
+//     const [user, setUser] = useState({});
+//     const [loading, setLoading] = useState(true)
+//     const auth = getAuth();
+//     const googleProvider = new GoogleAuthProvider();
 
-    const signInUsingGoogle = () => {
-        return signInWithPopup(auth, googleProvider)
-            .finally(() => { setLoading(false) });
-    }
+//     const signInUsingGoogle = () => {
+//         return signInWithPopup(auth, googleProvider)
+//             .finally(() => { setLoading(false) });
+//     }
 
-    const logOut = () => {
-        setLoading(true);
-        signOut(auth)
-            .then(() => {
-                setUser({})
-            })
-            .finally(() => setLoading(false))
-    }
+//     const logOut = () => {
+//         setLoading(true);
+//         signOut(auth)
+//             .then(() => {
+//                 setUser({})
+//             })
+//             .finally(() => setLoading(false))
+//     }
 
-    // observe whether user auth state changed or not
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                getIdToken(user)
-                    .then(idToken => localStorage.setItem('idToken', idToken));
-                setUser(user);
-            }
-            else {
-                setUser({});
-            }
-            setLoading(false);
-        });
-        return () => unsubscribe;
-    }, [])
+//     // observe whether user auth state changed or not
+//     useEffect(() => {
+//         const unsubscribe = onAuthStateChanged(auth, (user) => {
+//             if (user) {
+//                 getIdToken(user)
+//                     .then(idToken => localStorage.setItem('idToken', idToken));
+//                 setUser(user);
+//             }
+//             else {
+//                 setUser({});
+//             }
+//             setLoading(false);
+//         });
+//         return () => unsubscribe;
+//     }, [])
 
-    return {
-        user,
-        loading,
-        signInUsingGoogle,
-        logOut
-    }
-}
+//     return {
+//         user,
+//         loading,
+//         signInUsingGoogle,
+//         logOut
+//     }
+// }
 
-export default useFirebase;
+// export default useFirebase;
