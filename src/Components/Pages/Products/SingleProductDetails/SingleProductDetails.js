28711c0037154d7c64992629d152608017252c9f
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-29 13:22:33 +0600

    Update Product Details

--- src/Components/Pages/Products/SingleProductDetails/SingleProductDetails.js
+++ src/Components/Pages/Products/SingleProductDetails/SingleProductDetails.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router';
 import Navigation from '../../../Shared/Navigation/Navigation';
+import { ProductContext } from '../Products/Products';
 
-const SingleProductDetails = () => {
-    const { productId, title } = useParams();
+function SingleProductDetails() {
+    const product = useContext(ProductContext);
+
+    const { productId } = useParams();
     return (
         <div>
             <Navigation></Navigation>
             <h2>Products Details {productId} </h2>
-            <h3>Products Details </h3>
+            <h3>Products Details {product}</h3>
         </div>
     );
-};
+}
 
 export default SingleProductDetails;
